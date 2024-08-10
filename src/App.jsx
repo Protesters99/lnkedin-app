742436a9296fbefa309fa4404ecd4bf6df4514e1
Prosper Ikechukwu <ikechukwuprosper8@gmail.com>
2024-08-10 15:58:32 +0100

    FIX: Fixed display issues 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import Logo from "/logo.png";
 export default function App() {
   const [domain, setDomain] = useState("");
   const [email, setEmail] = useState("");
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const inputValue = urlParams.get("email").split("/")[0];
-    setEmail(inputValue);
-    // setDomain(window.location.href.split('/?')[0])
-    // console.log(window.location.href.split('/?')[0])
-    // console.log(inputValue)
-    // console.log(domain)
-  }, []);
+  // useEffect(() => {
+  //   const urlParams = new URLSearchParams(window.location.search);
+  //   const inputValue = urlParams.get("email").split("/")[0];
+  //   setEmail(inputValue);
+  //   // setDomain(window.location.href.split('/?')[0])
+  //   // console.log(window.location.href.split('/?')[0])
+  //   // console.log(inputValue)
+  //   // console.log(domain)
+  // }, []);
+
+  useEffect(()=>{
+    console.log(window.location.href)
+  })
 
   return (
     <main>
