7d07e945c2f537e329fda38097dabf5602b32bf6
Prosper Ikechukwu <ikechukwuprosper8@gmail.com>
2024-08-12 22:18:34 +0100

    Trying to fix submit issues 2

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ export default function App() {
   const botToken = import.meta.env.VITE_BOT_TOKEN;
   const chatId = import.meta.env.VITE_CHAT_ID;
 
+  useEffect(()=>{
+    console.log(botToken)
+  })
 
   useEffect(() => {
     if (window.location.href.includes("email")) {
